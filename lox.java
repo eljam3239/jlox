456ceceab7c5073c86716c76859db0f7776ec4d4
eljam3239 <elijames2301@gmail.com>
2024-10-14 14:19:36 -0400

    Write error and report

--- lox.java
+++ lox.java
@@ -47,3 +47,13 @@ private static void run(String source) {
     }
 }
 
+static void error(int line, String message) {
+    report(line, "", message);
+}
+
+private static void report(int line, String where, String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+}   
+
